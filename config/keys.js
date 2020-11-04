46e6834ae01db9d7d8f3c0b449fefa800de0c616
Skúli Arnarsson <skuli.arnarsson@advania.is>
2020-11-04 15:50:45 +0000

    try to get server running

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,9 @@
 // keys.js - figure out what set of keys to return
 if (process.env.NODE_ENV === 'production') {
+    console.log('PRODUCTION KEYS IN USE')
     // In production - return prod set of keys
     module.exports = require('./prod');
+    console.log('GOOGLE CLIENT ID: ' process.env.GOOGLE_CLIENT_ID);
 } else {
     // In development - return dev set of keys
     module.exports = require('./dev');
